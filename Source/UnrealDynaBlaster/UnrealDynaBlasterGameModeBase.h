1a1794aa0acb2934c115bca19206d36b3f1293d4
hassan <hosseini_hasan@hotmail.com>
2018-01-11 21:28:30 +0330

    Creation of empty C++ project

---
+++ Source/UnrealDynaBlaster/UnrealDynaBlasterGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "UnrealDynaBlasterGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALDYNABLASTER_API AUnrealDynaBlasterGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
